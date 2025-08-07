586bc2c064d63ae0dd90ef4a3360afd5b4ab2380
Rishikesh P C <rishikeshpc4545@gmail.com>
2025-08-07 22:32:57 +0530

    getting input and stored

--- javascript.js
+++ javascript.js
@@ -1,13 +1,26 @@
-let buttons = document.querySelectorAll('.buttons')
-let num1;
-let operator;
-let num2;
-let num;
+let num1="";
+let operator="";
+let num2="";
 
-buttons.forEach(e => {
-    e.addEventListener('click', (element)=> {
-        num = element.target.innerHTML
+document.querySelectorAll('.num').forEach(e => {
+    e.addEventListener('click', ()=> {
+        if (!operator) {
+            num1 += e.textContent
+            console.log("num1",num1);
+        }else {
+            num2 += e.textContent
+            console.log("num2",num2);
+        }
 
+    })
+})
+
+document.querySelectorAll('.operator').forEach(e => {
+    e.addEventListener('click',()=> {
+        if (num1) {
+            operator = e.textContent
+            console.log("ope",operator);
+    }
     })
 })
 
