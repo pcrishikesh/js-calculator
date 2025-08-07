3b5655b509753405ebf23690489d53eeeacce6bc
Rishikesh P C <rishikeshpc4545@gmail.com>
2025-08-07 22:37:12 +0530

    updating textbox with btns

--- javascript.js
+++ javascript.js
@@ -2,14 +2,16 @@ let num1="";
 let operator="";
 let num2="";
 
+let textbox = document.querySelector('.textbox')
+
 document.querySelectorAll('.num').forEach(e => {
     e.addEventListener('click', ()=> {
         if (!operator) {
             num1 += e.textContent
-            console.log("num1",num1);
+            textbox.value = num1
         }else {
             num2 += e.textContent
-            console.log("num2",num2);
+            textbox.value = num2
         }
 
     })
@@ -19,8 +21,9 @@ document.querySelectorAll('.operator').forEach(e => {
     e.addEventListener('click',()=> {
         if (num1) {
             operator = e.textContent
-            console.log("ope",operator);
+            textbox.value = operator
     }
     })
 })
+
 
