36bcd7c6c0fb6eddf040657f5a14a915a983716e
Rishikesh pc <94773767+pcrishikesh@users.noreply.github.com>
2026-01-16 01:46:48 +0530

    added the multiple addition sub.. fnc

--- javascript.js
+++ javascript.js
@@ -1,16 +1,16 @@
-let num1="";
-let operatorvalue="";
-let num2="";
+let num1 = "";
+let operatorvalue = "";
+let num2 = "";
 let result = "";
 
 let textbox = document.querySelector('.textbox')
 
 document.querySelectorAll('.num').forEach(e => {
-    e.addEventListener('click', ()=> {
+    e.addEventListener('click', () => {
         if (!operatorvalue) {
             num1 += e.textContent
             textbox.value = num1
-        }else {
+        } else {
             num2 += e.textContent
             textbox.value = num2
             console.log(operatorvalue);
@@ -20,13 +20,13 @@ document.querySelectorAll('.num').forEach(e => {
 })
 
 document.querySelectorAll('.operator').forEach(e => {
-    e.addEventListener('click',()=> {
+    e.addEventListener('click', () => {
         if (num1) {
             operatorvalue = e.textContent
             textbox.value = operatorvalue
-            if (operatorvalue!="")
-                operatorvalue = e.textContent            
-    }
+            if (operatorvalue != "")
+                operatorvalue = e.textContent
+        }
 
     })
 })
@@ -34,16 +34,16 @@ document.querySelectorAll('.operator').forEach(e => {
 
 function operate(fnumber, operator, snumber) {
     if (operator == "+") {
-        result = add(fnumber,snumber)
+        result = add(fnumber, snumber)
     }
-    else if(operate=="-") {
-        result = subtract(fnumber,snumber)
+    else if (operate == "-") {
+        result = subtract(fnumber, snumber)
     }
-    else if(operate=="*") {
-        result =  multiply(fnumber,snumber)
+    else if (operate == "*") {
+        result = multiply(fnumber, snumber)
     }
-    else if(operate=="*") {
-        result = divide(fnumber,snumber)
+    else if (operate == "*") {
+        result = divide(fnumber, snumber)
     }
     else {
         result = alert("wrong input");
@@ -52,29 +52,48 @@ function operate(fnumber, operator, snumber) {
 
 function equate() {
     let equatorbtn = document.querySelector('.equate')
-    equatorbtn.addEventListener('click', ()=> {
-        if (num1==="" || num2 === "" || operatorvalue === "") {
+    equatorbtn.addEventListener('click', () => {
+        if (num1 === "" || num2 === "" || operatorvalue === "") {
             alert("give correct input")
-        }else {
-            operate(parseInt(num1),operatorvalue,parseInt(num2))
+        } else {
+            operate(parseInt(num1), operatorvalue, parseInt(num2))
             console.log(result);
-            
+
+            document.querySelectorAll('.operator').forEach(e => {
+                e.addEventListener('click', () => {
+                    if (num1) {
+                        operatorvalue = e.textContent
+                        textbox.value = operatorvalue
+                        num1 = result
+                        if (num1 == result) {
+                            num2 = ""
+                        }
+
+
+                    }
+
+                })
+            })
+            console.log(`result ${num1}`)
+            textbox.value = result
+
         }
     })
+
 }
 
 equate()
 
 
-function add(a,b) {
+function add(a, b) {
     return a + b;
 }
 
-function subtract(a,b) {
+function subtract(a, b) {
     return a - b;
 }
 
-function divide(a,b) {
+function divide(a, b) {
     return a / b;
 }
 
